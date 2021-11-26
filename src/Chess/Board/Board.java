ceeb0232b8a773fc1dc24ed31d4f1e1a5cf37fc3
Joao-Arthur <joao.lothamer@gmail.com>
2021-11-26 20:58:28 -0300

    toggle board click

--- src/Chess/Board/Board.java
+++ src/Chess/Board/Board.java
@@ -1,9 +1,51 @@
 package Board;
 
 import java.awt.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 import javax.swing.*;
 
 public class Board extends JPanel {
+    Point currentClick;
+    Point lastClick;
+
+    public Board() {
+        addMouseListener(new MouseAdapter() {
+            @Override
+            public void mousePressed(MouseEvent e) {
+                lastClick = currentClick;
+                currentClick = e.getPoint();
+                repaint();
+            }
+        });
+    }
+
+    private boolean isValueInLimits(int point, int initialValue, int finalValue) {
+        return point >= initialValue && point <= finalValue;
+    }
+
+    private boolean isPointInDimension(double value, int dimension, int size) {
+        return isValueInLimits((int) value, dimension, dimension + size);
+    }
+
+    private boolean isPointClicked(int x, int y, int size, Point point) {
+        return isPointInDimension(point.getX(), x, size) && isPointInDimension(point.getY(), y, size);
+    }
+
+    private boolean isSquareClicked(int x, int y, int size) {
+        if (currentClick == null)
+            return false;
+        final var isCurrentClicked = isPointClicked(x, y, size, currentClick);
+        if (lastClick == null)
+            return isCurrentClicked;
+        final var isLastClicked = isPointClicked(x, y, size, lastClick);
+        if (isLastClicked && isCurrentClicked) {
+            currentClick = null;
+            return false;
+        }
+        return isCurrentClicked;
+
+    }
 
     @Override
     protected void paintComponent(Graphics g) {
@@ -29,10 +71,14 @@ public class Board extends JPanel {
                 } else {
                     drawer.setPaint(BoardColors.LIGHT);
                 }
+
                 darkSquare = !darkSquare;
 
                 final var x = paddingX + boardHeightIndex * squareSize;
                 final var y = paddingY + boardWidthIndex * squareSize;
+
+                if (isSquareClicked(x, y, squareSize))
+                    drawer.setPaint(BoardColors.FOCUS);
 
                 drawer.fillRect(x, y, squareSize, squareSize);
             }
