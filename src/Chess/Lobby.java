8182be4d497e176a83e2047d926a40ec541a0ba8
Joao-Arthur <joao.lothamer@gmail.com>
2021-11-25 22:05:19 -0300

    create lobby screen

--- src/Chess/Lobby.java
+++ src/Chess/Lobby.java
@@ -1,14 +1,57 @@
-import javax.swing.JFrame;
+import java.awt.*;
+import java.util.Date;
+import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
 
 public class Lobby extends JFrame {
-    JFrame LobbyScreen;
 
     public Lobby() {
-        LobbyScreen = new JFrame();
-        LobbyScreen.setVisible(true);
-        LobbyScreen.setSize(1366, 768);
-        LobbyScreen.setTitle("Lobby | Socket Chess");
-        LobbyScreen.setLocationRelativeTo(null);
-        LobbyScreen.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        final var lobbyScreen = createLobbyScreen();
+        lobbyScreen.add(createSidebar(), BorderLayout.EAST);
+        lobbyScreen.add(createMatchesTable());
+    }
+
+    private JFrame createLobbyScreen() {
+        final var lobbyScreen = new JFrame();
+        lobbyScreen.setVisible(true);
+        lobbyScreen.setSize(1366, 768);
+        lobbyScreen.setTitle("Lobby | Socket Chess");
+        lobbyScreen.setLocationRelativeTo(null);
+        lobbyScreen.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        return lobbyScreen;
+    }
+
+    private JPanel createSidebar() {
+        final var sidebar = new JPanel();
+        sidebar.add(createSidebarButton("criar partida"));
+        sidebar.add(createSidebarButton("conectar à partida"));
+        return sidebar;
+    }
+
+    private JButton createSidebarButton(String title) {
+        final var sidebarButton = new JButton(title);
+        sidebarButton.setSize(200, 30);
+        return sidebarButton;
+    }
+
+    private JTable createMatchesTable() {
+        final String data[][] = { { "João", new Date().toString(), "17636" },
+                { "Pedro", new Date().toString(), "17636" }, { "Marcos", new Date().toString(), "17636" },
+                { "João", new Date().toString(), "17636" }, { "Pedro", new Date().toString(), "17636" }, };
+        final String column[] = { "Nome do jogador", "Hora criação", "Porta" };
+
+        final var matchesTable = new JTable();
+        matchesTable.setModel(new DefaultTableModel(data, column) {
+            @Override
+            public boolean isCellEditable(int rowIndex, int columnIndex) {
+                return false;
+            }
+        });
+        matchesTable.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+
+        matchesTable.setAutoCreateRowSorter(true);
+        matchesTable.getTableHeader().setReorderingAllowed(false);
+        matchesTable.setVerifyInputWhenFocusTarget(false);
+        return matchesTable;
     }
 }
