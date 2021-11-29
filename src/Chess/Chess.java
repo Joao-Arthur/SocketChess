c697c7d582b28353a44e4cd20ea7058762c90be0
João Arthur <joao.arthur@emasoftware.com.br>
2021-11-29 12:45:36 -0300

    todos

--- src/Chess/Chess.java
+++ src/Chess/Chess.java
@@ -3,3 +3,12 @@ public class Chess {
         WindowManager.getInstance().goToLobby();
     }
 }
+
+
+/* TODO
+    - separar movimentos das peças nos serviços específicos 
+    - enviar movimentos pelo socket
+    - pegar os movimentos do socket e enviar pro serviço de atualizar a peça
+    - implementar pelo menos check e check mate
+    - criar serviço comum para verificar se está em check ou check mate
+*/
